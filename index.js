4a7071fdaa62276971bdc3dc02c17a0f601a2e7b
MichelleKamau6 <michwk06@gmail.com>
2025-06-29 20:04:43 +0300

    Document

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 function titleCased() {
   return tutorials.map(function (title) {
     return title
-      .split(" ")
+      .split(" ") // Split the string into words
       .map(function (word) {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); 
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); // Capitalize first letter
       })
-      .join(" ");
+      .join(" "); // Join words back into a sentence
   });
 }
+

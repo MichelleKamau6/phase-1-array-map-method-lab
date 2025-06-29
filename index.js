c4fd9809b949012db7f8856efbd287f62692b5c8
MichelleKamau6 <michwk06@gmail.com>
2025-06-29 21:43:33 +0300

    Document2

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const tutorials = [
 
 function titleCased() {
   return tutorials.map(function (sentence) {
-    let words = sentence.split(" ");
-    let capitalizedWords = words.map(function (word) {
+    const words = sentence.split(" ");
+    const capitalizedWords = words.map(function (word) {
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     });
     return capitalizedWords.join(" ");
   });
 }
+
+console.log(titleCased());
